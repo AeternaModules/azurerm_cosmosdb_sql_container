4637a4058a0aa37c7335a03bb5c147b339afce53
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:41 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -12,11 +12,11 @@ output "cosmosdb_sql_containers_analytical_storage_ttl" {
 }
 output "cosmosdb_sql_containers_autoscale_settings" {
   description = "Map of autoscale_settings values across all cosmosdb_sql_containers, keyed the same as var.cosmosdb_sql_containers"
-  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => v.autoscale_settings if v.autoscale_settings != null && length(v.autoscale_settings) > 0 }
+  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => one(v.autoscale_settings) if v.autoscale_settings != null && length(v.autoscale_settings) > 0 }
 }
 output "cosmosdb_sql_containers_conflict_resolution_policy" {
   description = "Map of conflict_resolution_policy values across all cosmosdb_sql_containers, keyed the same as var.cosmosdb_sql_containers"
-  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => v.conflict_resolution_policy if v.conflict_resolution_policy != null && length(v.conflict_resolution_policy) > 0 }
+  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => one(v.conflict_resolution_policy) if v.conflict_resolution_policy != null && length(v.conflict_resolution_policy) > 0 }
 }
 output "cosmosdb_sql_containers_database_name" {
   description = "Map of database_name values across all cosmosdb_sql_containers, keyed the same as var.cosmosdb_sql_containers"
@@ -28,7 +28,7 @@ output "cosmosdb_sql_containers_default_ttl" {
 }
 output "cosmosdb_sql_containers_indexing_policy" {
   description = "Map of indexing_policy values across all cosmosdb_sql_containers, keyed the same as var.cosmosdb_sql_containers"
-  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => v.indexing_policy if v.indexing_policy != null && length(v.indexing_policy) > 0 }
+  value       = { for k, v in azurerm_cosmosdb_sql_container.cosmosdb_sql_containers : k => one(v.indexing_policy) if v.indexing_policy != null && length(v.indexing_policy) > 0 }
 }
 output "cosmosdb_sql_containers_name" {
   description = "Map of name values across all cosmosdb_sql_containers, keyed the same as var.cosmosdb_sql_containers"
